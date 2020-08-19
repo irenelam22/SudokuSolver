b970f70e5487d828a57cd9fae0555b3c138f2852
Irene X. Lam 22 <ilam@moose.cs.dartmouth.edu>
2020-08-18 20:02:04 -0400

    David PG header file for checking row, column, unit

---
+++ common/movechecker.h
@@ -0,0 +1,11 @@
+// if an error was detected say so
+void analyze(char* ptr, int a, int b);
+
+//check the row
+bool row_checker(int puzzle, int N);
+
+//check the column
+bool columner_checker(int puzzle, int N);
+
+//check the unit
+bool unit_checker(int puzzle, int N);
