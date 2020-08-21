be5c3a2d00e23fddd437277d7f8bc0f25a99bfad
Irene X. Lam 22 <ilam@moose.cs.dartmouth.edu>
2020-08-21 19:44:43 -0400

    Beginning unit testing for solve, modified makefiles, cleaned up directory

--- solve/solve_test.c
+++ solve/solve_test.c
@@ -4,8 +4,8 @@
 
 int main(const int argc, const char *argv[])
 {
-    puzzle_t *puzzle = puzzle_new(); 
-    unit_t* cell = choose_unit(puzzle);
+    // puzzle_t *puzzle = puzzle_new(); 
+    // unit_t* cell = choose_unit(puzzle);
     return 0;
 }
 
