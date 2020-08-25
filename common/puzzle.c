f8ae00bde2e7a452ad439a3332d29ea2eaeb5d71
Kelly Westkaemper <kelly.b.westkaemper.22@dartmouth.edu>
2020-08-25 19:43:17 -0400

    updated puzzle.c

--- common/puzzle.c
+++ common/puzzle.c
@@ -1,29 +1,32 @@
 /* 
-
-file: puzzle.c – implementation for puzzle class
-assignment: final project – sudoku
-group: project-kidd
-
+puzzle.c – implementation for puzzle class
+The puzzle initializes a puzzle struct, essentially a 2D array of units
+in a 9x9 grid. The following implementations instantiate/maintain/delete this
+puzzle. These methods include a creation, iteration, pretty printing, finding
+the next available unit, updating the possibles list, and deleting the puzzle.
+Team KIDD - Kelly Westkaemper, Irene Lam, David Kantor, David Perez Gonzalez
+Dartmouth CS50, Summer 2020
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <ctype.h>
 #include "puzzle.h"
 #include "./unit.h"
 #include "../libcs50/file.h"
 #include "../libcs50/memory.h"
 
-
+// Constant instantiation
 const int MAX_ROW = 9; 
 const int MAX_COL = 9; 
-const int UNIT_SIZE = 32;  // Note: sizeof(unit_t) wasn't working, so I hard-coded it as a global variable
+const int UNIT_SIZE = 32;  
 const int EMPTY_CELL = 0;
 
 /*******puzzle_new********/
 /* Create new empty puzzle, where all values are 0
-*/
+ * Inputs: none
+ * Outputs: new puzzle
+ */
 puzzle_t* puzzle_new()
 {
     // Allocate space for entire puzzle 
@@ -34,11 +37,8 @@ puzzle_t* puzzle_new()
         // Allocate space for each row 
         puzzle[i] = assertp(malloc(MAX_COL*UNIT_SIZE), "puzzle row malloc failed"); 
         
-        for (int j = 0; j < MAX_COL; j++) {
-            /* note: it would be easy to initialize each 
-               unit with a given row, col number --> don't 
-               need all the math / extra work in unit_new */
-            
+        // Instantiate a unit for each of the 9x9 spaces within the puzzle
+        for (int j = 0; j < MAX_COL; j++) {            
             puzzle[i][j] = unit_new(unit_num, EMPTY_CELL);
             unit_num++; 
         }
@@ -47,7 +47,8 @@ puzzle_t* puzzle_new()
 }
 
 /*************puzzle_print*************/
-/* prints puzzle like so: 
+/* Printing method
+    prints puzzle like so: 
     0 0 0 | 0 0 0 | 0 0 0 
     0 0 0 | 0 0 0 | 0 0 0 
     0 0 0 | 0 0 0 | 0 0 0 
@@ -59,13 +60,14 @@ puzzle_t* puzzle_new()
     0 0 0 | 0 0 0 | 0 0 0 
     0 0 0 | 0 0 0 | 0 0 0 
     0 0 0 | 0 0 0 | 0 0 0 
+* Inputs: 
+    - file to print to
+    - puzzle to be printed
+* Ouputs: none (prints directly to file)
 */
 void puzzle_print(FILE *fp, puzzle_t *puzzle)
 {
     for (int i = 0; i < MAX_ROW; i++) {
-        /* note: if we want different size sudokus, 
-           we'll have to fix these hard-coded numbers 
-        */
         if (i == 3 || i == 6) {
             fprintf(fp, "---------------------\n"); 
         }
@@ -76,7 +78,6 @@ void puzzle_print(FILE *fp, puzzle_t *puzzle)
         fprintf(fp, "\n");
     }
 }
-
 
 /**********puzzle_load*************/
 /* Takes a pointer to a file that contains a properly formatted
@@ -137,7 +138,6 @@ puzzle_t *puzzle_load(FILE *fp)
     return puzzle; 
 }
 
-
 /**********puzzle_get_unit*************/
 /* Returns the unit of a puzzle given its row and col number
  * Inputs: puzzle, row number, col number of desired unit
@@ -160,6 +160,15 @@ unit_t* puzzle_get_unit(puzzle_t* puzzle, int row, int col)
     return puzzle[row][col];
 }
 
+/********* puzzle_iterate ************/
+/* Iterate method for the puzzle, calls the provided itemfunc method on each
+ * unit of the puzzle
+ * Inputs:
+ *  - puzzle file to iterate through
+ *  - pointer arg
+ *  - itemfunc to call on each unit
+ * Output: none (dependent on provided itemfunc)
+ */
 void puzzle_iterate(puzzle_t* puzzle, void *arg, void (*itemfunc)(void *arg, unit_t* cell))
 {
     for (int i = 0; i < MAX_ROW; i++) {
@@ -170,6 +179,10 @@ void puzzle_iterate(puzzle_t* puzzle, void *arg, void (*itemfunc)(void *arg, uni
 }
 
 /*********puzzle_delete************/
+/* Delete the provided puzzle and free necessary memory
+ * Input: puzzle to be deleted
+ * Output: none
+ */
 void puzzle_delete(puzzle_t *puzzle)
 {
     // Delete all units
@@ -197,6 +210,7 @@ void first_valid_unit(void* ptr, unit_t* current_cell)
     if (unit->possibles != NULL) {
         return;
     }
+    // Instantiate values of unit pointer
     if (current_cell -> val == 0) {
         unit -> val = current_cell -> val;
         unit -> unit_num = current_cell -> unit_num;
@@ -209,17 +223,23 @@ void first_valid_unit(void* ptr, unit_t* current_cell)
 
 /*******next_unit********/
 /* Finds the first unit within a puzzle not yet filled in
- * Inputs: puzzle
- * Output: unit
+ * Inputs: puzzle to find the next unit
+ * Output: next available unit
 */
 unit_t* next_unit(puzzle_t* puzzle) 
 {
+    // Instantiate a dummy unit
     unit_t* cell = unit_new(1, 0);
+
+    // Free unused memory
     counters_delete(cell->possibles);
     cell->possibles = NULL;
+
+    // Iterate to find the first valid unit
     puzzle_iterate(puzzle, cell, first_valid_unit);
+
+    // If the cell is empty, delete the allocated memory from above 
     if (cell->possibles == NULL) {
-        // If the cell is empty, delete the allocated memory from above 
         delete_unit(cell); 
         return NULL;
     }
@@ -234,11 +254,14 @@ unit_t* next_unit(puzzle_t* puzzle)
 */
 counters_t* possibles_create(puzzle_t* puzzle, unit_t* unit) 
 {
+    // Populate with numbers 1 through 9
     counters_t* set = counters_new();
     for (int i = 1; i < 10; i++) {
         counters_add(set, i);
     }
     unit -> possibles = set;
+
+    // Update the possibles list based on row/col/box values
     updating_possibles(puzzle, unit);
     return set;
 }
@@ -268,6 +291,7 @@ void updating_possibles_helper(void *arg, unit_t* unit)
         return;
     }
     unit_t* curr_unit = arg;
+
     // Values of unit we're comparing with
     int curr_row = curr_unit -> row_num;
     int curr_col = curr_unit -> col_num;
@@ -277,6 +301,8 @@ void updating_possibles_helper(void *arg, unit_t* unit)
     int temp_row = unit -> row_num;
     int temp_col = unit -> col_num;
     int temp_box = unit -> box_num;
+
+    // Remove from the possibles list if the value appears in the row/col/box
     if (curr_row == temp_row || curr_col == temp_col || curr_box == temp_box) {
         possibles_remove(curr_unit, unit -> val);
     }
@@ -300,7 +326,7 @@ void solveable_helper(void* ptr, unit_t* current_cell)
 /* Checks whether a puzzle is solveable (e.g. all units have a possible value)
  * Inputs: puzzle
  * Output: true if solveable, false otherwise
-*/
+ */
 bool is_puzzle_solveable(puzzle_t* puzzle) 
 {
     bool solveable = true;
