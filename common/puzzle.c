290828d7b3af2a9606c5391c94daf7d4d96076a2
Irene X. Lam 22 <ilam@moose.cs.dartmouth.edu>
2020-08-27 21:28:45 -0400

    Preparing for merge round 2

--- common/puzzle.c
+++ common/puzzle.c
@@ -101,57 +101,49 @@ puzzle_t *puzzle_load(FILE *fp)
     int unit_num = 1; 
     int val; 
     char *line;
-    int count = 0;
 
     // Go through entire file, line by line 
     while ((line = freadlinep(fp)) != NULL) {
-        count = 0;
         // Skip empty lines, or lines that start with "-"
-        if (strlen(line) == 0 || line[0] == 45) {
+        if (strlen(line) == 0 || line[0] == 45) {               // 45 is the asci code for "-"
             free(line); 
             continue;
+        }
+         if ( strlen(line) > 22 ){                              // length of properly formatted line
+            fprintf(stderr, "Invalid puzzle format\n"); 
+            puzzle_delete(puzzle);
+            free(line);
+		    return NULL; 
         }
 
         // Otherwise, process the line
         for (int i = 0; i < strlen(line); i++) {
             // If character is a digit
+
             if (isdigit(line[i])) {
                 // If the next character is also a digit, then the format is invalid
-                if (i+1<strlen(line) && isdigit(line[i+1])) {
+                if (i + 1 < strlen(line) && isdigit(line[i + 1])) {
                     fprintf(stderr, "Invalid puzzle format\n"); 
                     puzzle_delete(puzzle);
                     free(line);
 		            return NULL; 
                 }
-                // Subtract out 48 to get actual digit value
+                // Subtract out 48 to get actual digit value (48 is the asci code for "0")
                 val = line[i] - 48;
                 // Insert into puzzle, and increment unit_num and column
                 puzzle[row][col] -> val = val;
                 unit_num++; 
                 col++; 
-                count++;
             }
             // If character is not a space, or "|", then the format is invalid
-            else if (line[i] != 32 && line[i] != 124) {
+            else if (line[i] != 32 && line[i] != 124) { // 32 and 124 are the asci codes for " " and "|" 
                 fprintf(stderr, "Invalid puzzle format\n"); 
                 puzzle_delete(puzzle);
                 free(line);
 		        return NULL; 
             }
-            else if (isspace(line[i])) {
-                continue;
-            }
-            else {
-                count++;
-            }
         }
         // Free memory, and move to the next row
-        if (count != 11) {
-            fprintf(stderr, "Row %d of file invalid\n", row);
-            free(line);
-            clean_incomplete_puzzle(puzzle, row, col, unit_num);
-            return NULL;
-        }
         free(line);
         row++; 
         col = 0; 
